84ead588860f86a1db0b7657d56afcaf5320c8b1
kazumakano <kazumakano@outlook.jp>
2023-07-12 14:40:24 +0900

    modify loss weight bug

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def train(gpu_id: int, param_file: str, ts_fig_dir: str, ckpt_file: Optional[str
 
     if ckpt_file is None:
         datamodule.setup("fit")
-        model = CNN3(param, torch.from_numpy(len(datamodule.dataset["train"].label) / datamodule.dataset["train"].breakdown))
+        model = CNN3(param, torch.from_numpy(len(datamodule.dataset["train"].label) / datamodule.dataset["train"].breakdown).to(dtype=torch.float32))
         trainer.fit(model, datamodule=datamodule)
         model.load_from_checkpoint(glob(path.join(trainer.log_dir, "checkpoints/", "epoch=*-step=*.ckpt"))[0], loss_weight=torch.empty(10, dtype=torch.float32))
     else:
