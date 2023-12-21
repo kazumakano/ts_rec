243693bc397e78d4670dabade92e1e59e3fb596d
kazumakano <kazumakano@outlook.jp>
2023-12-21 16:22:13 +0900

    change prop name in VidDataset4ManyFrms

--- script/data.py
+++ script/data.py
@@ -74,7 +74,7 @@ class VidDataset4ManyFrms(VidDataset):
         self.cam_name = path.basename(path.dirname(file))[6:]
         file_name = path.basename(file)
         self.vid_idx = int(file_name[15:-4])
-        self.label_at_first_frm = util.calc_ts_from_name(file_name, sec_per_file) if label_at_start_frm is None else label_at_start_frm
+        self.label_at_start_frm = util.calc_ts_from_name(file_name, sec_per_file) if label_at_start_frm is None else label_at_start_frm
 
         frms = util.read_head_n_frms(file, max_frm_num, self.start_frm_idx)
         self.img = torch.empty((len(frms), 6, 3, 22, 17), dtype=torch.float32)
