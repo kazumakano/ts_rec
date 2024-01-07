e612a5a6690c83967989df72318116df4ab67ac3
kazumakano <kazumakano@outlook.jp>
2024-01-07 15:47:02 +0900

    refactor encode.sh

--- encode.sh
+++ encode.sh
@@ -3,11 +3,12 @@ if [ $# -lt 2 ] || [ $# -gt 3 ]; then
     exit
 fi
 
-for f in $(find $1/camera$3*/video_??-??-??_*.mkv); do
-    dir_name=$(basename $(dirname $f))
-    file_name=$(basename $f)
-
-    echo "encoding $dir_name/${file_name:0:-4}.mp4"
-    ffmpeg -y -i $f -vcodec h264_nvenc -vf fps=5 $2/$dir_name/${file_name:0:-4}.mp4 2>> $2/log.txt
-    echo >> $2/log.txt
+for d in $(find $1/camera$3* -maxdepth 0); do
+    dir_name=$(basename $d)
+    for f in $(find $d/video_??-??-??_*.mkv); do
+        file_name=$(basename $f)
+        echo "encoding $(basename $d)/${file_name:0:-4}.mp4"
+        ffmpeg -y -i $f -vcodec h264_nvenc -vf fps=5 $2/$dir_name/${file_name:0:-4}.mp4 2>> $2/log.txt
+        echo >> $2/log.txt
+    done
 done
