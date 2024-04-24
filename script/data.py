4d03f47db71108c3d92616ad7e0229b414b887b1
kazumakano <kazumakano@outlook.jp>
2024-04-24 12:39:30 +0900

    minor update

--- script/data.py
+++ script/data.py
@@ -24,7 +24,7 @@ class CsvDataset(data.Dataset):
         df = pd.read_csv(csv_file, usecols=("cam", "vid_idx", "recog", "is_smudged"))
         df = df.loc[df.loc[:, "vid_idx"] == vid_idx]
         vid_files = glob(path.join(vid_dir, f"camera{df.iloc[0]['cam']}/video_??-??-??_{vid_idx:02d}.mp4"))
-        if len(vid_files) > 1:
+        if len(vid_files) != 1:
             raise Exception("video index must be unique")
         cap = cv2.VideoCapture(filename=vid_files[0])
         if cap.get(cv2.CAP_PROP_FRAME_COUNT) != len(df):
