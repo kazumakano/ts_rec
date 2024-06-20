e7cc6b3c2702ed4d1289dac01a7b73580d7e8bac
kazumakano <kazumakano@outlook.jp>
2024-06-20 15:40:48 +0900

    update compile_model.py

--- compile_model.py
+++ compile_model.py
@@ -1,15 +1,18 @@
 import os.path as path
 from glob import glob
 import torch
+import script.model as M
 import script.utility as util
-from script.model import CNN3
 
 
 def compile_model(result_dir: str, ver: int = 0) -> None:
-    CNN3.load_from_checkpoint(
+    param = util.load_param(path.join(result_dir, f"version_{ver}/hparams.yaml"))
+    M.get_model_cls(param["arch"]).load_from_checkpoint(
         glob(path.join(result_dir, f"version_{ver}/checkpoints/epoch=*-step=*.ckpt"))[0],
-        loss_weight=torch.empty(10, dtype=torch.float32) if util.load_param(path.join(result_dir, f"version_{ver}/hparams.yaml"))["enable_loss_weight"] else None
-    ).to(device=torch.device("cuda", 0)).to_torchscript(file_path=path.join(result_dir, f"version_{ver}/model.pt"))
+        map_location=torch.device("cuda", 0),
+        param=param,
+        loss_weight=torch.empty(10, dtype=torch.float32) if param["enable_loss_weight"] else None
+    ).to_torchscript(file_path=path.join(result_dir, f"version_{ver}/model.pt"), method="trace", example_inputs=torch.empty((1, 3, 22, 17)))
 
 if __name__ == "__main__":
     import argparse
