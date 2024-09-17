08c467c0fe2710fa982bd8aa23c481cee774f662
kazumakano <kazumakano@outlook.jp>
2024-09-17 21:35:55 +0900

    add position checker

---
+++ PositionChecker.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import requests as req
+import streamlit as st
+import script.utility as util
+
+URI = "http://192.168.175.120/action/snap?cam=0&user=admin&pwd=12345"
+
+def render() -> None:
+    st.title("Position Checker")
+
+    for c, i in zip(st.columns(6), util.extract_ts_fig(cv2.imdecode(np.frombuffer(req.get(URI).content, np.uint8), cv2.IMREAD_COLOR))):
+        with c:
+            st.image(i, use_column_width=True, channels="BGR")
+
+if __name__ == "__main__":
+    render()
