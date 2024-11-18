2a4762b0f5eb6969eede0f1042c7337705c9577b
kazumakano <kazumakano@outlook.jp>
2024-11-18 23:07:56 +0900

    fix cache bug

--- create_cache.py
+++ create_cache.py
@@ -7,16 +7,16 @@ from tqdm import tqdm
 
 CAM_NAMES = ("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8")
 
-def create(src_dir: str, tgt_file: str) -> None:
+def create(src_dir: str, tgt_file: str, ver: int) -> None:
     begin_dict, cache_dict = {}, {}
     for n in CAM_NAMES:
         results = []
-        for f in iglob(path.join(src_dir, n, f"??-??-??_*/version_*/interp_results.csv")):
+        for f in sorted(iglob(path.join(src_dir, n, f"??-??-??_*/version_{ver}/interp_results.csv"))):
             results.append(pd.read_csv(f, usecols=("cam", "vid_idx", "frm_idx", "interp")))
         results = pd.concat(results)
         results.loc[:, "interp"] = results.loc[:, "interp"].map(util.str2sec)
 
-        begin, end = results.loc[results.loc[:, "vid_idx"] == results.loc[:, "vid_idx"].min()].iloc[:5]["interp"].max(), results.loc[results.loc[:, "vid_idx"] == results.loc[:, "vid_idx"].max()].iloc[-5:]["interp"].min()
+        begin, end = results.iloc[:5]["interp"].max(), results.iloc[-5:]["interp"].min()
         cache = []
         for t in tqdm(range(begin, end + 1), desc=f"building cache for camera {n}"):
             results_of_t = results.loc[results.loc[:, "interp"] == t]
@@ -34,8 +34,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--src_dir", required=True, help="specify source directory", metavar="PATH_TO_SRC_DIR")
-    parser.add_argument("-t", "--tgt_file", required=True, help="specify target file", metavar="PATH_TO_TGT_FILE")
+    parser.add_argument("-s", "--src_dir", required=True, help="specify source result directory", metavar="PATH_TO_SRC_DIR")
+    parser.add_argument("-t", "--tgt_file", required=True, help="specify target cache file", metavar="PATH_TO_TGT_FILE")
+    parser.add_argument("-v", "--ver", default=0, type=int, help="specify result version", metavar="VER")
     args = parser.parse_args()
 
-    create(args.src_dir, args.tgt_file)
+    create(args.src_dir, args.tgt_file, args.ver)
