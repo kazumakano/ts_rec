effa093c0c6a8f8c79188f566f8a6a76997034eb
kazumakano <kazumakano@outlook.jp>
2025-02-27 15:58:34 +0900

    minor modify

--- PositionChecker.py
+++ PositionChecker.py
@@ -9,7 +9,7 @@ URI = "http://192.168.175.120/action/snap?cam=0&user=admin&pwd=12345"
 def render() -> None:
     st.title("Position Checker")
 
-    for c, i in zip(st.columns(6), util.extract_ts_fig(cv2.imdecode(np.frombuffer(req.get(URI).content, np.uint8), cv2.IMREAD_COLOR))):
+    for c, i in zip(st.columns(6), util.extract_ts_fig(cv2.imdecode(np.frombuffer(req.get(URI).content, dtype=np.uint8), cv2.IMREAD_COLOR))):
         with c:
             st.image(i, use_column_width=True, channels="BGR")
 
